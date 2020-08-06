f7d79675021e01fdd896f01b89b481ed9f72d44a
JDysiewicz <joshuadysiewicz@googlemail.com>
2020-08-06 12:45:15 +0100

    Previous worked fine on localhost but Not Found error - hopefully this is more successful

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ app.use("/api", userApiRouter);
 // app.use("/home", homeRouter);
 app.use("/api/messages", messagesApiRouter);
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    res.sendFile(path.join(__dirname + './client/build/index.html'))
 });
 
 module.exports = app;
